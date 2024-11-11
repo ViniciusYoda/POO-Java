2b22f162025111c31988582ca137271aa57bc897
Vinícius Yoda <vyoda4604@gmail.com>
2024-11-11 10:33:43 -0300

    Métodos Getter, Setter e Construtor

---
+++ aula04b/src/aula04/Aula04.java
@@ -0,0 +1,9 @@
+package aula04;
+
+public class Aula04 {
+    public static void main(String[] args) throws Exception {
+        Caneta c1 = new Caneta("NIC",  0.4f, "Amarelo");  
+
+        System.out.println(c1.getCor());
+    }
+}
